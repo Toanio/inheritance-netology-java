46bd8845985d9207e187bb61a9e8a08536280d1d
Toanio <44195177+Toanio@users.noreply.github.com>
2020-08-27 18:16:12 +0300

    Добавил код из лекции в мастер

--- src/main/java/ru/netology/domain/Product.java
+++ src/main/java/ru/netology/domain/Product.java
@@ -1,33 +1,66 @@
 package ru.netology.domain;
 
-
+import java.util.Objects;
 
 public class Product {
-    private int id;
-    private String name;
-    private String cost;
+  private int id;
+  private String name;
+  private int price;
 
-    public int getId() {
-        return id;
-    }
+  public Product() {
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public Product(int id, String name, int price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public String getCost() {
-        return cost;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setCost(String cost) {
-        this.cost = cost;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Product product = (Product) o;
+    return id == product.id &&
+        price == product.price &&
+        Objects.equals(name, product.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, price);
+  }
+
+  @Override
+  public String toString() {
+    return "Product{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", price=" + price +
+        '}';
+  }
 }
--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -3,33 +3,40 @@ package ru.netology.repository;
 import ru.netology.domain.Product;
 
 public class ProductRepository {
-    private Product[] products = new Product[0];
+  private Product[] items = new Product[0];
 
-    public void save(Product product) {
-        int length = products.length + 1;
-        Product[] tmp = new Product[length];
+  public void save(Product item) {
+    int length = items.length + 1;
+    Product[] tmp = new Product[length];
+    System.arraycopy(items, 0, tmp, 0, items.length);
+    int lastIndex = tmp.length - 1;
+    tmp[lastIndex] = item;
+    items = tmp;
+  }
 
-        System.arraycopy(products, 0, tmp, 0, products.length);
+  public Product[] findAll() {
+    return items;
+  }
 
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = product;
-        products = tmp;
+  public Product findById(int id) {
+    for (Product item : items) {
+      if (item.getId() == id) {
+        return item;
+      }
     }
-    public void removeById(int id){
-        int length = products.length - 1;
-        Product[] tmp = new Product[length];
-        int index = 0;
-        for (Product product : products) {
-            if (product.getId() != id) {
-                tmp[index] = product;
-                index++;
-            }
-        }
-        products = tmp;
-    }
+    return null;
+  }
 
-    public Product[] getAll(){
-        return products;
+  public void removeById(int id) {
+    int length = items.length - 1;
+    Product[] tmp = new Product[length];
+    int index = 0;
+    for (Product item : items) {
+      if (item.getId() != id) {
+        tmp[index] = item;
+        index++;
+      }
     }
-
+    items = tmp;
+  }
 }
